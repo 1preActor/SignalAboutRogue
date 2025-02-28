cff5d8a75b98f92681e321c33b9bc16c62fc869c
preActor <preactgithub@gmail.com>
2025-02-28 15:10:53 +0300

    init

--- Assets/AlarmVolume.cs
+++ Assets/AlarmVolume.cs
@@ -5,72 +5,76 @@ using System.Collections;
 public class AlarmVolume : MonoBehaviour
 {
     [SerializeField] private float _fadeSpeed = 1f;
-    [SerializeField] private float _permissibleError = 0.01f;
+    [SerializeField] private float _volumeTolerance = 0.01f;
 
     private AudioSource _alarmSound;
-    private float _targetVolume;
+    private IntruderDetector _detector;
     private Coroutine _volumeChangeCoroutine;
-    private float _startVolume;
 
     private void Awake()
-    {
+    {    
         _alarmSound = GetComponent<AudioSource>();
+        _detector = GetComponent<IntruderDetector>();
+
         _alarmSound.volume = 0f;
     }
 
-    private void Start()
+    private void OnEnable()
     {
-        IntruderDetector detector = GetComponent<IntruderDetector>();
-
-        if (detector != null)
+        if (_detector != null)
         {
-            detector.OnIntruderEntered += OnIntruderEntered;
-            detector.OnIntruderExited += OnIntruderExited;
+            _detector.OnIntruderEntered += OnIntruderEntered;
+            _detector.OnIntruderExited += OnIntruderExited;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_detector != null)
+        {
+            _detector.OnIntruderEntered -= OnIntruderEntered;
+            _detector.OnIntruderExited -= OnIntruderExited;
+        }
+
+        if (_volumeChangeCoroutine != null)
+        {
+            StopCoroutine(_volumeChangeCoroutine);
+            _volumeChangeCoroutine = null;
         }
     }
 
     private void OnIntruderEntered()
     {
-        _targetVolume = 1f;
-
-        if (_volumeChangeCoroutine == null)
-        {
-            _startVolume = _alarmSound.volume;
-            _volumeChangeCoroutine = StartCoroutine(ChangeVolume());
-        }
+        StartVolumeChange(1f);
     }
 
     private void OnIntruderExited()
     {
-        _targetVolume = 0f;
-
-        if (_volumeChangeCoroutine == null)
-        {
-            _startVolume = _alarmSound.volume;
-            _volumeChangeCoroutine = StartCoroutine(ChangeVolume());
-        }
+        StartVolumeChange(0f);
     }
 
-    private IEnumerator ChangeVolume()
-    {
-        float volume = Mathf.Abs(_alarmSound.volume - _targetVolume);
-
-        while (volume > _permissibleError)
-        {
-            _alarmSound.volume = Mathf.MoveTowards(_alarmSound.volume, _targetVolume, _fadeSpeed * Time.deltaTime);
-            yield return null;
-        }
-
-        _alarmSound.volume = _targetVolume;
-
-        _volumeChangeCoroutine = null;
-    }
-
-    private void OnDestroy()
+    private void StartVolumeChange(float targetVolume)
     {
         if (_volumeChangeCoroutine != null)
         {
             StopCoroutine(_volumeChangeCoroutine);
         }
+
+        _volumeChangeCoroutine = StartCoroutine(ChangeVolume(targetVolume));
+    }
+
+    private IEnumerator ChangeVolume(float targetVolume)
+    {
+        float startVolume = _alarmSound.volume;
+
+        while (Mathf.Abs(_alarmSound.volume - targetVolume) > _volumeTolerance)
+        {
+            _alarmSound.volume = Mathf.MoveTowards(_alarmSound.volume, targetVolume, _fadeSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        _alarmSound.volume = targetVolume;
+
+        _volumeChangeCoroutine = null;
     }
 }
